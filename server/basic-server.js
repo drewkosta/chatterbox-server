54de24193f52cb3445e1b8295707144590773199
drewkosta <drewkosta@gmail.com>
2016-08-16 12:14:25 -0700

    Implement express refactor

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,24 @@
 /* Import node's http module: */
 var http = require('http');
 var handleRequest = require('./request-handler');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var responseBody = {
+  results: []
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/classes/messages', function (req, res) {
+  res.send(200, responseBody);
+});
+
+app.post('/classes/messages', function (req, res) {
+  responseBody.results.push(req.body);
+  res.send(201, responseBody);
+});
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
@@ -9,22 +27,22 @@ var handleRequest = require('./request-handler');
 // ports are 8080 and 1337.
 var port = 3000;
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
-var ip = '127.0.0.1';
+// // For now, since you're running this server on your local machine,
+// // we'll have it listen on the IP address 127.0.0.1, which is a
+// // special address that always refers to localhost.
+// var ip = '127.0.0.1';
 
-
-
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
+// // We use node's http module to create a server.
+// //
+// // The function we pass to http.createServer will be used to handle all
+// // incoming requests.
+// //
+// // After creating the server, we will tell it to listen on the given port and IP. */
+// var server = http.createServer(handleRequest);
+// console.log('Listening on http://' + ip + ':' + port);
+app.listen(port, function () {
+  console.log('Listening on ' + port);
+});
 
 // To start this server, run:
 //
